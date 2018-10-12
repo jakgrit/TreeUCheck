ffacb151a2e3ca5e3220ddc895180415dfd6e7c2
jakgrit <jakgrit_frame@hotmail.com>
2018-10-12 20:40:01 +0700

    edit mail

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -29,6 +29,6 @@ class SendMail extends Mailable
      */
     public function build(request $request)
     {
-        return $this->view('mail',['msg'=>$request->message])->to('jakgritframe@gmail.com')->subject($request->head);
+        return $this->view('mail',['msg'=>$request->message])->to('59160002@go.buu.ac.th')->subject($request->head);
     }
 }
